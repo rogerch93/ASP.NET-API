d9ba9420046d8b444ea48e07c6ab04d67e7e8bf8
rogerch93 <rogerinhochaves@hotmail.com>
2021-08-29 20:17:14 -0300

    Criando API e Adc Projeto MVC nela

---
+++ MVC-DIO/MVC/MVC/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MVC;Integrated Security=True");
+        }
+    }
+}
